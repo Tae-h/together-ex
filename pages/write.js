d2a0dabd6600fe42b590e8c7556a5b88cb1a4735
siyoon <siyoon@ebrainsoft.com>
2022-07-11 13:29:22 +0900

    [together]  input, textarea 추가

--- pages/write.js
+++ pages/write.js
@@ -1,8 +1,40 @@
 import {memo} from "react";
+import React from 'react';
 import AppLayout from "../components/AppLayout";
 import Head from "next/head";
+import styled from 'styled-components';
 import {Layout} from "antd";
 const { Content, Footer } = Layout;
+import { Input } from 'antd';
+const { TextArea } = Input;
+const onChange = (e) => {
+    console.log('Change:', e.target.value);
+};
+
+
+import {
+    LeftOutlined,
+} from "@ant-design/icons";
+
+const headerWrite = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+}
+const headerBtnWrap = {
+    position: 'unset',
+}
+const headerTitle = {
+    margin: '0',
+    fontSize: '1.125rem',
+    fontWeight: 'bold',
+}
+const headerBtn = {
+    border: 'none',
+    backgroundColor: 'transparent',
+    fontSize: '1rem',
+    fontWeight: '700',
+}
 
 const Write = memo(() => {
 
@@ -10,15 +42,25 @@ const Write = memo(() => {
     return (
         <>
             <AppLayout>
-                <Head>
-                    <title>
-                        글 작성 | 운동할래?
-                    </title>
-                </Head>
+                <div className="header" style={{...headerWrite}}>
+                    <div className="prev-btn">
+                        <LeftOutlined />
+                    </div>
+                    <p className="title" style={{...headerTitle}}>
+                        글 제목
+                    </p>
+                    <div className="btn-wrap" style={{...headerBtnWrap}}>
+                        <button style={{...headerBtn}}>완료</button>
+                    </div>
+                </div>
 
                 <Content>
-
-                    글쓰기
+                    <div className="input-box">
+                        <Input showCount placeholder="제목" maxLength={40} onChange={onChange} />
+                    </div>
+                    <div className="input-box">
+                        <TextArea showCount placeholder='내용을 입력해주세요' maxLength={100} onChange={onChange} />
+                    </div>
                 </Content>
             </AppLayout>
         </>
